9ff42d868155befe76cc2e65ac12b5557513309e
AyeshaZahid296 <ayeshazahid296@gmail.com>
2025-09-02 21:20:58 +0500

    post api done

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/products.js';
 
 dotenv.config();
 
 const app = express();
 
-app.post("/products", (req, res) => {
+app.use(express.json());
+
+app.post("/api/products", async (req, res) => {
+    const product = await req.body;
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please fill all fields" })
+    }
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        return res.status(201).json({ success: true, data: newProduct })
+    } catch (error) {
+        console.log("Error in created product", error.message)
+        return res.status(500).json({ success: false, massage: "Server Error" })
+    }
 });
+
+
 
 const PORT = process.env.PORT || 5000;
 
