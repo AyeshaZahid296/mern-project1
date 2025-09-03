bfdf541b26b7970a02d9e7c11d08ba2bc791b0cb
AyeshaZahid296 <ayeshazahid296@gmail.com>
2025-09-03 20:17:57 +0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/products.js';
+import productsRouter from './routes/productsRouter.js';
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // allow use to accept data in rq.body in json formate
 
-app.get("/api/products", async (req, res) => {
-    try {
-        const products = await Product.find({});
-        return res.status(200).json({ success: true, data: products })
-    } catch (error) {
-        console.log("Error in fetching products", error.massage);
-        return res.status(500).json({ success: false, message: "Server error" })
-    }
-})
-
-app.post("/api/products", async (req, res) => {
-    const product = await req.body;
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({ success: false, message: "Please fill all fields" })
-    }
-    const newProduct = new Product(product);
-
-    try {
-        await newProduct.save();
-        return res.status(201).json({ success: true, data: newProduct })
-    } catch (error) {
-        console.log("Error in created product", error.message)
-        return res.status(500).json({ success: false, massage: "Server Error" })
-    }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-    const { id } = req.params;
-
-    try {
-        const deletedProduct = await Product.findByIdAndDelete(id);
-
-        if (!deletedProduct) {
-            return res.status(400).json({ success: false, massage: "Product not found" })
-        }
-        return res.status(200).json({ success: true, massage: "Product Deleted sucessfully " })
-    } catch (error) {
-        console.log('Error in deleting product', error.message);
-        return res.status(500).json({ success: false, message: "This id is not matched" })
-    }
-})
+app.use("/api/products", productsRouter)
 
 const PORT = process.env.PORT || 5000;
 
