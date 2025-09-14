72e613b1271c1940001e3efe005a41656bfb5aa0
AyeshaZahid296 <ayeshazahid296@gmail.com>
2025-09-14 22:20:38 +0500

    toggle funcationality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from '@chakra-ui/react'
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import CreatePage from './pages/CreatePage'
+import HomePage from './pages/HomePage'
+import Navbar from './components/Navbar'
+
 const App = () => {
   return (
-    <div>
-      <Button> hello</Button>
-
+    <div className="min-h-screen transition-colors duration-500 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
+      <Navbar />
+      <h1 className="text-2xl font-bold p-4">Welcome to Product Store</h1>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/create' element={<CreatePage />} />
+      </Routes>
     </div>
   )
 }
